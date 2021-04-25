c81d99b8daf99a3539e4ab498471331d6c668c42
dapayne25@gmail.com <dapayne25@gmail.com>
2021-04-25 17:57:07 -0500

    Populated Sales

--- populateDataVINN.sql
+++ populateDataVINN.sql
@@ -17,6 +17,38 @@ INSERT INTO Sales.Account(UserName, UserPassword)
 SELECT * FROM Sales.Account;
 
 INSERT INTO Sales.Manager(ManagerId, FName, LName)
-		Values ('1','Billy', 'Bob'),
+		Values ('1', 'Billy', 'Bob')
 
 SELECT * FROM Sales.Manager;
+
+--INSERT INTO Sales.CreditCard(ManagerId, AccountId, AcctNum)
+--		Values ()
+
+--SELECT * FROM Sales.CreditCard;
+
+--INSERT INTO Sales.Receipt()
+--		Values()
+
+--SELECT * FROM Sales.Receipt;
+
+INSERT INTO Sales.Employee(FName, LName, JobTitle, DateHired, Salary, Specialty, WorkHistory)
+		Values('Tom', 'Clean', 'HouseKeeper', '1-1-2021', '35,000', 'Cleaning', 'None'),
+			('Billy', 'Office', 'Accounting', '1-2-2021', '55,000', 'Accounting', 'Yes'),
+			('William', 'Manager', 'Office Manager', '1-3-2021', '65,000', 'Managing', 'Yes'),
+			('Bob', 'Sweeper', 'HouseKeeper', '1-4-2021', '35,000', 'Cleaning', 'None')
+
+SELECT * FROM Sales.Employee;
+
+INSERT INTO Sales.EmpType(EmpId, TypeName)
+		Values('1', 'HouseKeeper'),
+			('2', 'Accounting'),
+			('3', 'Office Manager')
+
+SELECT * FROM Sales.EmpType;
+
+INSERT INTO Sales.WrkHist(EmpId, NewHire)
+		Values('5', '0')
+
+SELECT * FROM Sales.WrkHist;
+
+
